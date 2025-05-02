ec22aa88177091f218d08e60c58012d49eb2c3db
Dmitriy <Dmitry5335@yandex.ru>
2025-05-02 17:59:49 +0300

    feat: add realisation of shifting and rewinding

--- tape.cpp
+++ tape.cpp
@@ -1,4 +1,6 @@
 #include "tape.hpp"
+#include <thread>
+#include <chrono>
 
 Tape::Tape(size_t size, int moveDelay, int rewindDelay, int writeDelay, int readDelay):
   tape_(size, 0),
@@ -8,3 +10,21 @@ Tape::Tape(size_t size, int moveDelay, int rewindDelay, int writeDelay, int read
   writeDelay_(writeDelay),
   readDelay_(readDelay)
 {}
+
+void Tape::shiftLeft()
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(moveDelay_));
+  --iter_;
+}
+
+void Tape::shiftRight()
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(moveDelay_));
+  ++iter_;
+}
+
+void Tape::rewind()
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(rewindDelay_));
+  iter_ = tape_.begin();
+}
