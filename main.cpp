f87db39918a79b48b997851e3dae578dcf0ba831
Dmitriy <Dmitry5335@yandex.ru>
2025-05-02 17:26:53 +0300

    feat: add simple parameters input to main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 
-int main()
-{}
+int main(int argc, char* argv[])
+{
+  if (argc < 3)
+  {
+    std::cerr << "Error: Not enough arguments\n";
+    return 1;
+  }
+
+  std::string inputFile = argv[1];
+  std::string outputFile = argv[2];
+  std::string configFile = argv[3];
+}
