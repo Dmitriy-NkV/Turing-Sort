ea9711e40598459ff36c1e20b19c73deb62c2446
Dmitriy <Dmitry5335@yandex.ru>
2025-05-02 17:30:28 +0300

    feat: add constructor to Tape interface and rewind method

--- tape.hpp
+++ tape.hpp
@@ -6,17 +6,19 @@
 class Tape
 {
 public:
+  Tape(size_t size);
   virtual ~Tape() = default;
 
   virtual void shiftLeft() = 0;
   virtual void shiftRight() = 0;
   virtual int read() const = 0;
   virtual void write(int element) = 0;
+  virtual void rewind() = 0;
   virtual bool isEnd() const = 0;
 
 private:
-  std::vector< int >::iterator iter_;
   std::vector< int > tape_;
+  std::vector< int >::iterator iter_;
 };
 
 #endif
