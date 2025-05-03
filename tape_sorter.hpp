ce0103a1910698414e8f8d0e26883a821b4b9502
Dmitriy <Dmitry5335@yandex.ru>
2025-05-04 00:04:39 +0300

    feat: add tape_sorter class declaration

---
+++ tape_sorter.hpp
@@ -0,0 +1,21 @@
+#ifndef TAPE_SORTER_HPP
+#define TAPE_SORTER_HPP
+
+#include <iostream>
+#include <vector>
+#include "tape.hpp"
+
+class TapeSorter
+{
+public:
+  TapeSorter(const Config& config, size_t ram);
+  ~TapeSorter() = default;
+
+  void operator()(Tape& inputTape, Tape& outputTape, const Config& config) const;
+
+private:
+  Config config_;
+  size_t ram_;
+};
+
+#endif
