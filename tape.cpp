6176729240ab50048a5df7a0382f393c8a127581
Dmitriy <Dmitry5335@yandex.ru>
2025-05-03 23:59:36 +0300

    refactor: make new realisation of tape methods

--- tape.cpp
+++ tape.cpp
@@ -1,52 +1,66 @@
 #include "tape.hpp"
 #include <thread>
 #include <chrono>
+#include <iomanip>
+#include <iostream>
 
-Tape::Tape(size_t size, int moveDelay, int rewindDelay, int writeDelay, int readDelay):
-  tape_(size, 0),
-  iter_(tape_.begin()),
+Config::Config(int moveDelay, int rewindDelay, int writeDelay, int readDelay):
   moveDelay_(moveDelay),
   rewindDelay_(rewindDelay),
   writeDelay_(writeDelay),
   readDelay_(readDelay)
 {}
 
+Tape::Tape(const std::string& fileName, const Config& config):
+  tape_(fileName, std::ios::in | std::ios::out),
+  currentPos_(0),
+  config_(config)
+{}
+
 void Tape::shiftLeft()
 {
-  std::this_thread::sleep_for(std::chrono::milliseconds(moveDelay_));
-  --iter_;
+  std::this_thread::sleep_for(std::chrono::milliseconds(config_.moveDelay_));
+  currentPos_ -= 12;
 }
 
 void Tape::shiftRight()
 {
-  std::this_thread::sleep_for(std::chrono::milliseconds(moveDelay_));
-  ++iter_;
+  std::this_thread::sleep_for(std::chrono::milliseconds(config_.moveDelay_));
+  currentPos_ += 12;
 }
 
 void Tape::rewind()
 {
-  std::this_thread::sleep_for(std::chrono::milliseconds(rewindDelay_));
-  iter_ = tape_.begin();
+  std::this_thread::sleep_for(std::chrono::milliseconds(config_.rewindDelay_));
+  currentPos_ = 0;
+  tape_.seekg(currentPos_);
 }
 
 void Tape::write(int element)
 {
-  std::this_thread::sleep_for(std::chrono::milliseconds(writeDelay_));
-  *iter_ = element;
+  tape_.seekg(currentPos_);
+  std::this_thread::sleep_for(std::chrono::milliseconds(config_.writeDelay_));
+  tape_ << std::hex << std::setfill('0') 
+    << std::setw(2) << (element & 0xFF) << " "
+    << std::setw(2) << ((element >> 8) & 0xFF) << " "
+    << std::setw(2) << ((element >> 16) & 0xFF) << " "
+    << std::setw(2) << ((element >> 24) & 0xFF) << "\n";
 }
 
 int Tape::read() const
 {
-  std::this_thread::sleep_for(std::chrono::milliseconds(readDelay_));
-  return *iter_;
+  tape_.seekg(currentPos_);
+  std::this_thread::sleep_for(std::chrono::milliseconds(config_.readDelay_));
+  int byte0 = 0;
+  int byte1 = 0;
+  int byte2 = 0;
+  int byte3 = 0;
+
+  tape_ >> std::hex >> byte0 >> byte1 >> byte2 >> byte3;
+  return byte0 | (byte1 << 8) | (byte2 << 16) | (byte3 << 24);
 }
 
 bool Tape::isEnd() const
 {
-  return iter_ == tape_.end();
-}
-
-size_t Tape::size() const
-{
-  return tape_.size();
+  return tape_.eof();
 }
