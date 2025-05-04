7ff4ec20ccc470e348a5703330a259d7aae891b8
Dmitriy <Dmitry5335@yandex.ru>
2025-05-04 20:45:55 +0300

    refactor: clean test_tape

--- tests/test_tape.cpp
+++ tests/test_tape.cpp
@@ -2,8 +2,9 @@
 #include <gmock/gmock.h>
 #include <filesystem>
 #include "../src/tape.hpp"
+#include "../src/functions.hpp"
 
-class MockTape 
+class MockTape
 {
 public:
   MOCK_METHOD(void, read, (), ());
@@ -13,138 +14,109 @@ TEST(TapeTest, Initialization)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
+  clearDirectory("tmp");
 
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
-
-  Tape tape(testFile, testConfig);
+  Tape testTape(testFile, testConfig);
   EXPECT_TRUE(std::filesystem::exists(testFile));
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
 }
 
 TEST(TapeTest, WriteAndRead)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
+  clearDirectory("tmp");
 
-  Tape tape(testFile, testConfig);
-  tape.write(100);
-  EXPECT_EQ(tape.read(), 100);
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
+  Tape testTape(testFile, testConfig);
+  testTape.write(100);
+  EXPECT_EQ(testTape.read(), 100);
 }
 
 TEST(TapeTest, Shift)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
+  clearDirectory("tmp");
 
-  Tape tape(testFile, testConfig);
-  tape.write(100);
-  tape.shiftRight();
-  tape.write(200);
-  tape.shiftRight();
-  tape.write(300);
+  Tape testTape(testFile, testConfig);
+  testTape.write(100);
+  testTape.shiftRight();
+  testTape.write(200);
+  testTape.shiftRight();
+  testTape.write(300);
 
-  EXPECT_EQ(tape.read(), 300);
-  tape.shiftLeft();
-  EXPECT_EQ(tape.read(), 200);
-  tape.shiftLeft();
-  EXPECT_EQ(tape.read(), 100);
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
+  EXPECT_EQ(testTape.read(), 300);
+  testTape.shiftLeft();
+  EXPECT_EQ(testTape.read(), 200);
+  testTape.shiftLeft();
+  EXPECT_EQ(testTape.read(), 100);
 }
 
 TEST(TapeTest, Rewind)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
+  clearDirectory("tmp");
 
-  Tape tape(testFile, testConfig);
-  tape.write(100);
-  tape.shiftRight();
-  tape.write(200);
-  tape.shiftRight();
-  tape.write(300);
-  tape.rewind();
+  Tape testTape(testFile, testConfig);
+  testTape.write(100);
+  testTape.shiftRight();
+  testTape.write(200);
+  testTape.shiftRight();
+  testTape.write(300);
+  testTape.rewind();
 
-  EXPECT_EQ(tape.read(), 100);
-  tape.shiftRight();
-  EXPECT_EQ(tape.read(), 200);
-  tape.shiftRight();
-  EXPECT_EQ(tape.read(), 300);
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
+  EXPECT_EQ(testTape.read(), 100);
+  testTape.shiftRight();
+  EXPECT_EQ(testTape.read(), 200);
+  testTape.shiftRight();
+  EXPECT_EQ(testTape.read(), 300);
 }
 
 TEST(TapeTest, Rewrite)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
+  clearDirectory("tmp");
 
-  Tape tape(testFile, testConfig);
-  tape.write(100);
-  tape.shiftRight();
-  tape.write(200);
-  tape.shiftRight();
-  tape.write(300);
-  tape.shiftLeft();
-  tape.write(500);
-  tape.rewind();
+  Tape testTape(testFile, testConfig);
+  testTape.write(100);
+  testTape.shiftRight();
+  testTape.write(200);
+  testTape.shiftRight();
+  testTape.write(300);
+  testTape.shiftLeft();
+  testTape.write(500);
+  testTape.rewind();
 
-  EXPECT_EQ(tape.read(), 100);
-  tape.shiftRight();
-  EXPECT_EQ(tape.read(), 500);
-  tape.shiftRight();
-  EXPECT_EQ(tape.read(), 300);
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
+  EXPECT_EQ(testTape.read(), 100);
+  testTape.shiftRight();
+  EXPECT_EQ(testTape.read(), 500);
+  testTape.shiftRight();
+  EXPECT_EQ(testTape.read(), 300);
 }
 
 TEST(TapeTest, EndCheck)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
+  clearDirectory("tmp");
 
-  Tape tape(testFile, testConfig);
-  tape.write(100);
-  tape.shiftRight();
-  tape.write(100);
-  tape.shiftRight();
-  tape.write(100);
-  tape.rewind();
+  Tape testTape(testFile, testConfig);
+  testTape.write(100);
+  testTape.shiftRight();
+  testTape.write(100);
+  testTape.shiftRight();
+  testTape.write(100);
+  testTape.rewind();
 
-  MockTape tape1;
-  EXPECT_CALL(tape1, read())
+  MockTape mockTape;
+  EXPECT_CALL(mockTape, read())
     .Times(3);
 
-  while (!tape.isEnd())
+  while (!testTape.isEnd())
   {
-    tape1.read();
-    tape.shiftRight();
-  }
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
+    mockTape.read();
+    testTape.shiftRight();
   }
 }
 
@@ -152,30 +124,22 @@ TEST(TapeTest, OutOfRangeRead)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  Tape tape(testFile, testConfig);
+  clearDirectory("tmp");
 
+  Tape testTape(testFile, testConfig);
   EXPECT_THROW({
-    tape.read();
+    testTape.read();
   }, std::out_of_range);
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
 }
 
 TEST(TapeTest, OutOfRangeShift)
 {
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  Tape tape(testFile, testConfig);
+  clearDirectory("tmp");
 
+  Tape testTape(testFile, testConfig);
   EXPECT_THROW({
-    tape.shiftLeft();
+    testTape.shiftLeft();
   }, std::out_of_range);
-
-  if (std::filesystem::exists(testFile))
-  {
-    std::filesystem::remove(testFile);
-  }
 }
