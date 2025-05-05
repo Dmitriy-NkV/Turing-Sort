cf0e5046f9bdbd6955d2eb15c51db7adc191fece
Dmitriy <Dmitry5335@yandex.ru>
2025-05-05 14:37:27 +0300

    feat: add outputs to errors in tests

--- tests/test_tape.cpp
+++ tests/test_tape.cpp
@@ -4,38 +4,32 @@
 #include "../src/tape.hpp"
 #include "../src/functions.hpp"
 
-class MockTape
-{
-public:
-  MOCK_METHOD(void, read, (), ());
-};
-
 TEST(TapeTest, Initialization)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
-  EXPECT_TRUE(std::filesystem::exists(testFile));
+  EXPECT_TRUE(std::filesystem::exists(testFile)) << "Tape file does not created";
 }
 
 TEST(TapeTest, WriteAndRead)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
   testTape.write(100);
-  EXPECT_EQ(testTape.read(), 100);
+  EXPECT_EQ(testTape.read(), 100) << "Actual: " << testTape.read() << ", Expected: " << 100;
 }
 
 TEST(TapeTest, Shift)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
   testTape.write(100);
@@ -44,18 +38,18 @@ TEST(TapeTest, Shift)
   testTape.shiftRight();
   testTape.write(300);
 
-  EXPECT_EQ(testTape.read(), 300);
+  EXPECT_EQ(testTape.read(), 300) << "Actual: " << testTape.read() << ", Expected: " << 300;
   testTape.shiftLeft();
-  EXPECT_EQ(testTape.read(), 200);
+  EXPECT_EQ(testTape.read(), 200) << "Actual: " << testTape.read() << ", Expected: " << 200;
   testTape.shiftLeft();
-  EXPECT_EQ(testTape.read(), 100);
+  EXPECT_EQ(testTape.read(), 100) << "Actual: " << testTape.read() << ", Expected: " << 100;
 }
 
 TEST(TapeTest, Rewind)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
   testTape.write(100);
@@ -65,18 +59,18 @@ TEST(TapeTest, Rewind)
   testTape.write(300);
   testTape.rewind();
 
-  EXPECT_EQ(testTape.read(), 100);
+  EXPECT_EQ(testTape.read(), 100) << "Actual: " << testTape.read() << ", Expected: " << 100;
   testTape.shiftRight();
-  EXPECT_EQ(testTape.read(), 200);
+  EXPECT_EQ(testTape.read(), 200) << "Actual: " << testTape.read() << ", Expected: " << 200;
   testTape.shiftRight();
-  EXPECT_EQ(testTape.read(), 300);
+  EXPECT_EQ(testTape.read(), 300) << "Actual: " << testTape.read() << ", Expected: " << 300;
 }
 
 TEST(TapeTest, Rewrite)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
   testTape.write(100);
@@ -88,18 +82,24 @@ TEST(TapeTest, Rewrite)
   testTape.write(500);
   testTape.rewind();
 
-  EXPECT_EQ(testTape.read(), 100);
+  EXPECT_EQ(testTape.read(), 100) << "Actual: " << testTape.read() << ", Expected: " << 100;
   testTape.shiftRight();
-  EXPECT_EQ(testTape.read(), 500);
+  EXPECT_EQ(testTape.read(), 500) << "Actual: " << testTape.read() << ", Expected: " << 500;
   testTape.shiftRight();
-  EXPECT_EQ(testTape.read(), 300);
+  EXPECT_EQ(testTape.read(), 300) << "Actual: " << testTape.read() << ", Expected: " << 300;
 }
+
+class MockTape
+{
+public:
+  MOCK_METHOD(void, read, (), ());
+};
 
 TEST(TapeTest, EndCheck)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
   testTape.write(100);
@@ -122,24 +122,20 @@ TEST(TapeTest, EndCheck)
 
 TEST(TapeTest, OutOfRangeRead)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
-  EXPECT_THROW({
-    testTape.read();
-  }, std::out_of_range);
+  EXPECT_THROW({ testTape.read(); }, std::out_of_range) << "Expected: std::out_of_range";
 }
 
 TEST(TapeTest, OutOfRangeShift)
 {
+  clearDirectory("tmp");
   Config testConfig = Config(0, 0, 0, 0);
   const std::string testFile = "tmp/test_tape.txt";
-  clearDirectory("tmp");
 
   Tape testTape(testFile, testConfig);
-  EXPECT_THROW({
-    testTape.shiftLeft();
-  }, std::out_of_range);
+  EXPECT_THROW({ testTape.shiftLeft(); }, std::out_of_range) << "Expected: std::out_of_range";
 }
